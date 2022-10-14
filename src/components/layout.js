35393780b807d5e3f1892ba3814c9c820099e47d
Abdeljalil BENETTALEB <abdeljalil.benettaleb@um6p.ma>
2022-10-14 19:18:31 +0100

    Create a menu from wordpress

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
+import "@wordpress/block-library/build-style/style.css"
 import "../styles/layout.css"
 
 const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      wp {
+        generalSettings {
+          title
+          url
+        }
+      }
+      wpMenu(id: { eq: "dGVybToxNw==" }) {
+        menuItems {
+          nodes {
+            id
+            url
+            label
+          }
+        }
+      }
+    }
+  `)
+
+  const { title, url } = data.wp.generalSettings
+  const menuItems = data.wpMenu.menuItems.nodes.map(item => ({
+    ...item,
+    url: item.url.replace(url, ""),
+  }))
+
   return (
     <>
       <header>
         <Link to="/" className="home">
-          Migrate Wordpress to Jamstack
+          {title}
         </Link>
+
+        {menuItems.map(item => (
+          <Link to={item.url} key={item.id}>
+            {item.label}
+          </Link>
+        ))}
       </header>
       <main>{children}</main>
     </>
